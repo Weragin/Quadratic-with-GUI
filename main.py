eb7402abc029a4052f4b503f123458f7d361a0c4
Weragin <109145593+Weragin@users.noreply.github.com>
2023-04-11 14:38:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Quadratic")
+window.geometry("300x300")
+
+a_in = tk.Entry(text= "Parameter a")
+a_in.pack()
+
+b_in = tk.Entry(text= "Parameter b")
+b_in.pack()
+
+c_in = tk.Entry(text= "Parameter c")
+c_in.pack()
+
+
+def compute():
+  global a_in
+  global b_in
+  global c_in
+
+  a = int(a_in.get())
+  b = int(b_in.get())
+  c = int(c_in.get())
+
+  if b**2 > 4*a*c:
+    root1 = round((- b - (b**2 - 4*a*c)**(1/2))/2/a, 2)
+    root2 = round((- b + (b**2 - 4*a*c)**(1/2))/2/a, 2)
+    label.configure(text= f"The roots are {root1} and {root2}.")
+  elif b**2 == 4*a*c:
+    label.configure(text= f"The root is {round( - b/2/a, 2)}.")
+  else:
+    label.configure(text= "This equation doesn't have real roots.")
+  
+button = tk.Button(text= "Compute equation", command= compute).pack()
+
+label = tk.Label(text= "The results will be printed here.")
+label.configure()
+label.pack()
+
+tk.mainloop()
